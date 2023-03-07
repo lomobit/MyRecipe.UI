b00fcbf51471ccb7b02b05a1d7030f122409ceb9
vghslnk <vghslnk@vk.com>
2023-03-07 09:07:26 +0300

    Добавление диалогового окна для добавления ингредиентов

--- src/components/Ingredients/Ingredients.tsx
+++ src/components/Ingredients/Ingredients.tsx
@@ -1,112 +1,98 @@
-import Box from '@mui/material/Box';
 import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
+import UpdateIcon from '@mui/icons-material/Update';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import Pagination from '@mui/material/Pagination';
-import {
-    DataGrid,
-    GridColDef,
-    gridPageCountSelector,
-    gridPageSelector,
-    useGridApiContext,
-    useGridSelector
-} from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions } from '@mui/material';
 
-import './Ingredients.css';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { getAllIngredientsAsync, selectIngredients } from './ingredientSlice';
-import { columns, StyledGridOverlay } from './ingredientConstants';
+import { columns } from './ingredientConstants';
+import NoRowsGridOverlay from '../NoRowsGridOverlay/NoRowsGridOverlay';
+import MuiGridPagination from '../MuiGridPagination/MuiGridPagination';
 
-// TODO: Вынести эти 2 функции в отдельные компоненты
-function CustomNoRowsOverlay() {
-    return (
-      <StyledGridOverlay>
-        <svg
-          width="120"
-          height="100"
-          viewBox="0 0 184 152"
-          aria-hidden
-          focusable="false"
-        >
-          <g fill="none" fillRule="evenodd">
-            <g transform="translate(24 31.67)">
-              <ellipse
-                className="ant-empty-img-5"
-                cx="67.797"
-                cy="106.89"
-                rx="67.797"
-                ry="12.668"
-              />
-              <path
-                className="ant-empty-img-1"
-                d="M122.034 69.674L98.109 40.229c-1.148-1.386-2.826-2.225-4.593-2.225h-51.44c-1.766 0-3.444.839-4.592 2.225L13.56 69.674v15.383h108.475V69.674z"
-              />
-              <path
-                className="ant-empty-img-2"
-                d="M33.83 0h67.933a4 4 0 0 1 4 4v93.344a4 4 0 0 1-4 4H33.83a4 4 0 0 1-4-4V4a4 4 0 0 1 4-4z"
-              />
-              <path
-                className="ant-empty-img-3"
-                d="M42.678 9.953h50.237a2 2 0 0 1 2 2V36.91a2 2 0 0 1-2 2H42.678a2 2 0 0 1-2-2V11.953a2 2 0 0 1 2-2zM42.94 49.767h49.713a2.262 2.262 0 1 1 0 4.524H42.94a2.262 2.262 0 0 1 0-4.524zM42.94 61.53h49.713a2.262 2.262 0 1 1 0 4.525H42.94a2.262 2.262 0 0 1 0-4.525zM121.813 105.032c-.775 3.071-3.497 5.36-6.735 5.36H20.515c-3.238 0-5.96-2.29-6.734-5.36a7.309 7.309 0 0 1-.222-1.79V69.675h26.318c2.907 0 5.25 2.448 5.25 5.42v.04c0 2.971 2.37 5.37 5.277 5.37h34.785c2.907 0 5.277-2.421 5.277-5.393V75.1c0-2.972 2.343-5.426 5.25-5.426h26.318v33.569c0 .617-.077 1.216-.221 1.789z"
-              />
-            </g>
-            <path
-              className="ant-empty-img-3"
-              d="M149.121 33.292l-6.83 2.65a1 1 0 0 1-1.317-1.23l1.937-6.207c-2.589-2.944-4.109-6.534-4.109-10.408C138.802 8.102 148.92 0 161.402 0 173.881 0 184 8.102 184 18.097c0 9.995-10.118 18.097-22.599 18.097-4.528 0-8.744-1.066-12.28-2.902z"
-            />
-            <g className="ant-empty-img-4" transform="translate(149.65 15.383)">
-              <ellipse cx="20.654" cy="3.167" rx="2.849" ry="2.815" />
-              <path d="M5.698 5.63H0L2.898.704zM9.259.704h4.985V5.63H9.259z" />
-            </g>
-          </g>
-        </svg>
-        <Box sx={{ mt: 1 }}>Нет записей</Box>
-      </StyledGridOverlay>
-    );
-}
+import './Ingredients.css';
+import IconButton from '@mui/material/IconButton/IconButton';
+import { useState } from 'react';
 
-function CustomPagination() {
-    const apiRef = useGridApiContext();
-    const page = useGridSelector(apiRef, gridPageSelector);
-    const pageCount = useGridSelector(apiRef, gridPageCountSelector);
-  
-    return (
-      <Pagination
-        color="primary"
-        count={pageCount}
-        page={page + 1}
-        onChange={(event, value) => apiRef.current.setPage(value - 1)}
-      />
-    );
-}
 
 const Ingredients = () => {
     const ingredients = useAppSelector(selectIngredients);
     const dispatch = useAppDispatch();
 
+    const [openAddDialog, setOpenAddDialog] = useState(false);
+
+
+    const handleClickAddButton = () => {
+        setOpenAddDialog(true);
+    };
+    
+    const handleCancelInAddButtonDialog = () => {
+        setOpenAddDialog(false);
+    };
+
+    const handleAddInAddButtonDialog = () => {
+        setOpenAddDialog(false);
+    };
+
     return (
         <div className="ingredients">
             <h1>Ингредиенты</h1>
-            <Stack direction="row-reverse" spacing={1}>
+            <Stack direction="row-reverse" spacing={1} style={{ borderStyle: 'solid', border: '2px' }}>
                 <Button
                     variant="outlined"
                     startIcon={<EditIcon />}
-                    onClick={() => dispatch(getAllIngredientsAsync(1))}
-                    //disabled
+                    disabled
                 >
-                    (Обновить)Изменить
+                    Изменить
                 </Button>
                 <Button
                     variant="contained"
                     startIcon={<AddIcon />}
-                    onClick={() => {
-                        alert('Добавление');
-                    }
-                }>
+                    onClick={handleClickAddButton}
+                >
                     Добавить
                 </Button>
+                <IconButton
+                    color="primary"
+                    onClick={() => dispatch(getAllIngredientsAsync(1))}
+                    style={{marginLeft: 0}}
+                >
+                    <UpdateIcon />
+                </IconButton>
             </Stack>
+            <Dialog open={openAddDialog} onClose={handleCancelInAddButtonDialog}>
+                <DialogTitle>Добавить ингредиент</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Для добавление ингредиента введите данные:
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="Name"
+                        label="Название"
+                        variant="outlined"
+                        fullWidth
+                        required
+                        // error
+                        // helperText="Необходимо ввести имя"
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="Description"
+                        label="Описание"
+                        variant="outlined"
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCancelInAddButtonDialog}>Отмена</Button>
+                    <Button onClick={handleAddInAddButtonDialog}>Добавить</Button>
+                </DialogActions>
+            </Dialog>
             <div style={{ marginTop: 20, height: 994, width: '100%' }}>
                 <DataGrid
                     pagination
@@ -114,8 +100,8 @@ const Ingredients = () => {
                     rowsPerPageOptions={[25, 50]}
                     density='compact'
                     components={{
-                        NoRowsOverlay: CustomNoRowsOverlay,
-                        Pagination: CustomPagination,
+                        NoRowsOverlay: NoRowsGridOverlay,
+                        Pagination: MuiGridPagination,
                     }}
                     rows={ingredients.ingredients}
                     columns={columns}

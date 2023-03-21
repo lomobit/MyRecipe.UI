1d32336f0b52bcf0b815df7e1c98d86bddbb1b31
vghslnk <vghslnk@vk.com>
2023-03-21 16:47:04 +0300

    Добавление логики в кофигуратор

--- utils/configurator/src/configurator.ts
+++ utils/configurator/src/configurator.ts
@@ -1,1 +1,131 @@
-console.log(process.argv);
+/**
+ * Примеры использования:
+ *      1) node configurator --configPath="configs/.env.debug" --destinationPath="../../../.env" [--notUseParentConfigs]
+ *      2) node configurator --help
+ *
+ * --configPath= (*)
+ *      [ОБЯЗАТЕЛЬНЫЙ] Путь применяемой конфигурации приложения.
+ *
+ * --destinationPath= (*)
+ *      [ОБЯЗАТЕЛЬНЫЙ] Путь, куда будет складываться итоговая конфигурация после трансформации.
+ *
+ * --notUseParentConfigs
+ *      Параметр, указыавющий, что к применяемой трансформации, не нужно добавлять ключи из родительских трансформаций.
+ *
+ * --help
+ *      Сводка команд.
+ * 
+ */
+
+import fs from 'fs';
+import {
+    needHelpParameter,
+    notUseParentConfigsParameter,
+    configPathParameter,
+    destinationPathParameter,
+    errorsOutput,
+    errorValidationDuplicatedParameter,
+    errorValidationIncorrectUsingParameter,
+    errorValidationRequiredParameters, errorValidationUnknownParameters,
+    helpSummary,
+} from "./constants";
+
+
+if (process.argv.length < 3) {
+    console.error(errorValidationRequiredParameters([configPathParameter, destinationPathParameter]));
+}
+
+let needHelp: boolean = false;
+let notUseParentConfigs: boolean = false;
+let configPath: string = undefined;
+let destinationPath: string = undefined;
+
+let unknownArguments: Array<string> = [];
+let validationErrors: Array<string> = [];
+
+for (let i = 2; i < process.argv.length; i++) {
+    if (process.argv[i] === needHelpParameter) {
+        if (needHelp !== false) {
+            validationErrors.push(errorValidationDuplicatedParameter(needHelpParameter));
+            continue;
+        }
+
+        needHelp = true;
+    }
+    else if (process.argv[i] === notUseParentConfigsParameter) {
+        if (notUseParentConfigs !== false) {
+            validationErrors.push(errorValidationDuplicatedParameter(notUseParentConfigsParameter));
+            continue;
+        }
+
+        notUseParentConfigs = true;
+    }
+    else if (process.argv[i].startsWith(configPathParameter)) {
+        if (configPath !== undefined) {
+            validationErrors.push(errorValidationDuplicatedParameter(configPathParameter));
+            continue;
+        }
+
+        configPath = process.argv[i].substring(configPathParameter.length);
+    }
+    else if (process.argv[i].startsWith(destinationPathParameter)) {
+        if (destinationPath !== undefined) {
+            validationErrors.push(errorValidationDuplicatedParameter(destinationPathParameter));
+            continue;
+        }
+
+        destinationPath = process.argv[i].substring(destinationPathParameter.length);
+    }
+    else {
+        unknownArguments.push(process.argv[i]);
+    }
+}
+
+if (unknownArguments.length > 0) {
+    validationErrors.push(errorValidationUnknownParameters(unknownArguments));
+}
+
+if (validationErrors.length > 0) {
+    console.error(errorsOutput(validationErrors));
+    process.exit(-1);
+}
+
+if (needHelp) {
+    if (process.argv.length > 3) {
+        console.error(errorsOutput([errorValidationIncorrectUsingParameter(needHelpParameter)]));
+        process.exit(-1);
+    }
+    else {
+        console.info(helpSummary);
+        process.exit(0);
+    }
+}
+
+let emptyRequiredParameters: Array<string> = [];
+if (configPath === undefined || configPath === "") {
+    emptyRequiredParameters.push(configPathParameter);
+}
+
+if (destinationPath === undefined || destinationPath === "") {
+    emptyRequiredParameters.push(destinationPathParameter);
+}
+
+if (emptyRequiredParameters.length > 0) {
+    console.error(errorsOutput([errorValidationRequiredParameters(emptyRequiredParameters)]));
+    process.exit(-1);
+}
+
+if (notUseParentConfigs) {
+    try {
+        fs.copyFileSync(configPath, destinationPath);
+        console.log("Трансформация конфигурации успешно завершена.");
+        process.exit(0);
+    }
+    catch (ex) {
+        console.error(errorsOutput([ex]));
+        process.exit(-1);
+    }
+}
+
+console.log("Этого не должно быть видно");
+
---
+++ utils/configurator/src/constants.ts
@@ -0,0 +1,27 @@
+export const needHelpParameter = "--help";
+export const notUseParentConfigsParameter = "--notUseParentConfigs";
+export const configPathParameter = "--configPath=";
+export const destinationPathParameter = "--destinationPath=";
+
+export const errorValidationRequiredParameters = (parameters: Array<string>): string =>
+    `Не переданы обязательные параметры: ${parameters.join(", ")}. Для вызова справки используйте параметр ${needHelpParameter}.`;
+export const errorValidationUnknownParameters = (parameters: Array<string>): string =>
+    `Переданы неизвестные параметры: ${parameters.join(", ")}. Для вызова справки используйте параметр ${needHelpParameter}.`;
+export const errorValidationDuplicatedParameter = (parameter: string): string =>
+    `Многократное указание параметра: ${parameter}`;
+export const errorValidationIncorrectUsingParameter = (parameter: string): string =>
+    `Некорректное использование параметра: ${parameter}. Для вызова справки используйте параметр ${needHelpParameter}.`;
+export const errorsOutput = (errors: Array<string>): string =>
+    `[Ошибки]:\n\t- ${errors.join("\n\t- ")}`;
+
+export const helpSummary: string = `Примеры использования:
+    1) node configurator ${configPathParameter}"configs/.env.debug" ${destinationPathParameter}"../../../.env" [${notUseParentConfigsParameter}]
+    2) node configurator ${needHelpParameter}\n
+${configPathParameter} (*)
+    [ОБЯЗАТЕЛЬНЫЙ] Путь применяемой конфигурации приложения.\n
+${destinationPathParameter} (*)
+    [ОБЯЗАТЕЛЬНЫЙ] Путь, куда будет складываться итоговая конфигурация после трансформации.\n
+${notUseParentConfigsParameter}
+    Параметр, указыавющий, что к применяемой трансформации, не нужно добавлять ключи из родительских трансформаций.\n
+${needHelpParameter}
+    Сводка команд.`;

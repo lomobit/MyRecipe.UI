6360a5494c768c7cb67fd20e237329678409d309
vghslnk <vghslnk@vk.com>
2023-04-19 21:18:46 +0300

    Добавление верстки пагинации для страницы блюд

--- src/components/dishes/index.tsx
+++ src/components/dishes/index.tsx
@@ -3,8 +3,9 @@ import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
 import './index.css';
-import {Button, CardActionArea, IconButton, Stack, TextField} from '@mui/material';
+import {Button, CardActionArea, FormControl, IconButton, MenuItem, Select, Stack, TextField} from '@mui/material';
 import { mockDishes } from "./constants";
 import UpdateIcon from "@mui/icons-material/Update";
 import AddIcon from "@mui/icons-material/Add";
@@ -16,6 +17,7 @@ const Dishes = () => {
     return (
         <div className="dishes">
             <h1>Блюда</h1>
+            {/* TODO: блок кнопок для блюд повторяет блок кнопок для ингредиентов. Подумать на тем, нужно ли это вынести в отдельный компонент. */}
             <div className="dishesButtons">
                 <Stack direction="row" spacing={1} className="nameFilteringButtons">
                     <TextField
@@ -43,13 +45,12 @@ const Dishes = () => {
                     </Button>
                 </Stack>
             </div>
-            <Grid container justifyContent="flex-start" spacing={3}>
+            <Grid container justifyContent="flex-start" spacing={3} style={{marginBottom: 20}}>
                 {
                     mockDishes.map((dish) => (
                         <Grid key={dish.id} item>
                             <Card
                                 sx={{
-                                    marginTop: 2,
                                     width: 217
                                 }}
                                 onClick={() => alert(`${dish.name}`)}
@@ -77,6 +78,30 @@ const Dishes = () => {
                     ))
                 }
             </Grid>
+            <Stack direction="row-reverse" spacing={1}>
+                <Pagination
+                    color="primary"
+                    count={10}
+                    page={1}
+                    //onChange={handlePaginationChange}
+                    className="paginationPanel"
+                />
+                <FormControl
+                    sx={{ m: 1, minWidth: 60 }}
+                    size="small"
+                >
+                    <Select
+                        value={9}
+                        //onChange={handleSelectItemPerPageChange}
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Количество элементов' }}
+                    >
+                        <MenuItem value={9}>9</MenuItem>
+                        <MenuItem value={18}>18</MenuItem>
+                        <MenuItem value={27}>27</MenuItem>
+                    </Select>
+                </FormControl>
+            </Stack>
         </div>
     );
 }

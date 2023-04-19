d6fb6377a6f7aeea49a592c20f0aa1971303e05f
vghslnk <vghslnk@vk.com>
2023-04-19 20:54:50 +0300

    Изменение верстки страницы блюд

--- src/components/dishes/index.tsx
+++ src/components/dishes/index.tsx
@@ -4,8 +4,10 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import './index.css';
-import { CardActionArea } from '@mui/material';
+import {Button, CardActionArea, IconButton, Stack, TextField} from '@mui/material';
 import { mockDishes } from "./constants";
+import UpdateIcon from "@mui/icons-material/Update";
+import AddIcon from "@mui/icons-material/Add";
 
 const Dishes = () => {
 
@@ -14,37 +16,66 @@ const Dishes = () => {
     return (
         <div className="dishes">
             <h1>Блюда</h1>
-            <Grid container justifyContent="left" spacing={2}>
-                {mockDishes.map((value) => (
-                    <Grid key={value.id} item>
-                        <Card
-                            sx={{
-                                width: 217,
-                                backgroundColor: (theme) =>
-                                    theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-                            }}
-                        >
-                            <CardActionArea>
-                                <CardMedia
-                                    component="img"
-                                    width="100%"
-                                    height={140}
-                                    draggable={false}
-                                    image={`${process.env.REACT_APP_API_URL}/File/${value.dishPhotoGuid}`}
-                                    alt={`Фото ${value.name}`}
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        {value.name}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Количество персон: {value.numberOfPersons}
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
-                    </Grid>
-                ))}
+            <div className="dishesButtons">
+                <Stack direction="row" spacing={1} className="nameFilteringButtons">
+                    <TextField
+                        label="Поиск по названию"
+                        variant="outlined"
+                        size="small"
+                        //value={""}
+                        //onChange={onChangeNameFilter}
+                    />
+                    <IconButton
+                        aria-label="delete"
+                        color="primary"
+                        //onClick={handleClickUpdateButton}
+                    >
+                        <UpdateIcon />
+                    </IconButton>
+                </Stack>
+                <Stack className="addEditButtons">
+                    <Button
+                        variant="contained"
+                        startIcon={<AddIcon />}
+                        //onClick={handleClickAddButton}
+                    >
+                        Добавить
+                    </Button>
+                </Stack>
+            </div>
+            <Grid container justifyContent="flex-start" spacing={3}>
+                {
+                    mockDishes.map((dish) => (
+                        <Grid key={dish.id} item>
+                            <Card
+                                sx={{
+                                    marginTop: 2,
+                                    width: 217
+                                }}
+                                onClick={() => alert(`${dish.name}`)}
+                            >
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        width="100%"
+                                        height={140}
+                                        draggable={false}
+                                        image={`${process.env.REACT_APP_API_URL}/File/${dish.dishPhotoGuid}`}
+                                        alt={`Фото ${dish.name}`}
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {dish.name}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            Количество персон: {dish.numberOfPersons}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    ))
+                }
             </Grid>
         </div>
     );

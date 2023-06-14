a39f46eebf20aef24946916d45d685d24f5c18dd
lomobit <lomobit@gmail.com>
2023-06-14 18:56:57 +0300

    Добавлена новая реализация выбора файла для изображения блюда.

--- src/components/dishes/dishDialog/index.tsx
+++ src/components/dishes/dishDialog/index.tsx
@@ -10,7 +10,7 @@ import {
     TextField
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
-import ImagePicker from "../../image-picker";
+import {ChangeEvent, Fragment, useState} from "react";
 
 export declare interface DishesDialogProps {
     openDialog: boolean;
@@ -19,15 +19,42 @@ export declare interface DishesDialogProps {
 
 const DishesDialog = (props: DishesDialogProps) => {
 
+    const [file, setFile] = useState<File>();
+
+    const handleDishImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setFile(e.target.files[0]);
+        }
+    };
+
+    const noImageData = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0zMiAtMTIwIDYxMiA2MTIiPgogIDxwYXRoIGQ9Im0gNDggMzIgYyAwIDAgMCAwIDAgMCBsIDQ0OCAwIGMgMTYgMCAxNiAxNiAxNiAxNiB2IDI3MiBjIDAgMTYgLTE2IDE2IC0xNiAxNiBsIC00NDggMCBjIC0xNiAwIC0xNiAtMTYgLTE2IC0xNiB2IC0yNzIgYyAwIDAgMCAtMTYgMTYgLTE2IG0gLTE2IDE3NiBsIDExMiAtOTYgbCAxMjggMTI4IG0gMCAwIGwgOTYgLTk2IGwgMTQ0IDEyOCBtIC0xNDQgLTE5MSBDIDM3NiA4MiAzODEgODUgMzgyIDk1IEMgMzgwIDEwNSAzNzcgMTA4IDM2OCAxMDkgQyAzNjEgMTA4IDM1NiAxMDYgMzU0IDk1IEMgMzU1IDg2IDM2MCA4MiAzNjggODEiIHN0cm9rZT0iI2NlY2ZkMiIgc3Ryb2tlLXdpZHRoPSIxMCIgZmlsbD0iI2Y0ZjZmOSIvPgo8L3N2Zz4=";
+
     return (
         <Dialog open={props.openDialog} onClose={() => props.setOpenDialog(false)}>
             <DialogTitle>Добавить блюдо</DialogTitle>
             <DialogContent>
-                {/*<DialogContentText>*/}
-                {/*    Для добавление блюда введите данные:*/}
-                {/*</DialogContentText>*/}
 
-                <ImagePicker/>
+                <Fragment>
+                    <Fragment>
+                        <img
+                            src={file !== undefined
+                                ? URL.createObjectURL(file)
+                                : noImageData}
+                            width="100%"
+                            alt="There will be text"
+                        />
+                    </Fragment>
+                    <Button variant="outlined" component="label">
+                        Добавить изображение
+                        <input
+                            hidden={true}
+                            multiple={false}
+                            accept="image/*"
+                            type="file"
+                            onChange={handleDishImageChange}
+                        />
+                    </Button>
+                </Fragment>
 
 
                 <DialogContentText style={{marginTop: 30}}>

cca30392fc95fa858bc74fc0b8e51f55ded4131b
lomobit <lomobit@gmail.com>
2023-12-05 19:48:14 +0300

    Наклепал стилей для календаря

--- src/components/schedules/index.tsx
+++ src/components/schedules/index.tsx
@@ -1,139 +1,158 @@
 import './index.css';
-import React, { useState } from 'react';
+import {Fragment, useState} from "react";
+import {Button, FormControl, IconButton, MenuItem, Select, Stack} from '@mui/material';
+import * as React from "react";
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import AddIcon from "@mui/icons-material/Add";
 
-interface Event {
-    id: number;
-    title: string;
-    start: Date;
-    finish: Date;
+export declare interface EventsProps {
+    //currentDate: Date
 }
 
-const defaultEvent: Event = { id: 0, title: '', start: new Date(), finish: new Date() };
+function Events(props: EventsProps) {
+    const [currentDate, setCurrentDate] = useState<Date>(new Date());
 
-function Events() {
-    const [events, setEvents] = useState<Event[]>([]);
-    const [newEvent, setNewEvent] = useState<Event | null>(null);
-    const [selectedEvent, setSelectedEvent] = useState<Event | null>(null);
+    const currentPeriod = [
+        [{ month: 11, date: 27 }, { month: 11, date: 28 }, { month: 11, date: 29 }, { month: 11, date: 30 }, { month: 12, date: 1 }, { month: 12, date: 2 }, { month: 12, date: 3 } ],
+        [{ month: 12, date: 4 }, { month: 12, date: 5 }, { month: 12, date: 6 }, { month: 12, date: 7 }, { month: 12, date: 8 }, { month: 12, date: 9 }, { month: 12, date: 10 }],
+        [{ month: 12, date: 11 }, { month: 12, date: 12 }, { month: 12, date: 13 }, { month: 12, date: 14 }, { month: 12, date: 15 }, { month: 12, date: 16 }, { month: 12, date: 17 }],
+        [{ month: 12, date: 18 }, { month: 12, date: 19 }, { month: 12, date: 20 }, { month: 12, date: 21 }, { month: 12, date: 22 }, { month: 12, date: 23 }, { month: 12, date: 24 }],
+        [{ month: 12, date: 25 }, { month: 12, date: 26 }, { month: 12, date: 27 }, { month: 12, date: 28 }, { month: 12, date: 29 }, { month: 12, date: 30 }, { month: 12, date: 31 }]
+    ];
 
-    const handleDayClick = (date: Date) => {
-        setNewEvent({
-            ...defaultEvent,
-            id: Date.now(),
-            start: date,
-            finish: date,
-        });
-    };
+    const events = [
+        {
+            name: "Сплав1",
+            startAt: new Date(2023, 11, 26),
+            finishAt: new Date(2023, 12, 28)
+        },
+        {
+            name: "Сплав2",
+            startAt: new Date(2023, 12, 4),
+            finishAt: new Date(2023, 12, 6)
+        },
+        {
+            name: "Сплав3",
+            startAt: new Date(2023, 12, 5),
+            finishAt: new Date(2023, 12, 7)
+        },
+        {
+            name: "Сплав4",
+            startAt: new Date(2023, 12, 23),
+            finishAt: new Date(2023, 12, 26)
+        },
+        {
+            name: "Сплав5",
+            startAt: new Date(2023, 12, 30),
+            finishAt: new Date(2024, 1, 2)
+        },
+    ];
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (newEvent) {
-            setNewEvent((prevEvent) => {
-                if (!prevEvent) {
-                    return prevEvent; // Handle the case where prevEvent is null
-                }
-
-                return {
-                    ...prevEvent,
-                    [e.target.name]: e.target.name === 'start' || e.target.name === 'finish'
-                        ? new Date(e.target.value)
-                        : e.target.value,
-                };
-            });
-        }
-    };
-
-    const handleSaveEvent = () => {
-        if (newEvent) {
-            setEvents((prevEvents) => [...prevEvents, newEvent]);
-            setNewEvent(null);
-        }
-    };
-
-    const handleEventClick = (event: Event) => {
-        setSelectedEvent(event);
-    };
-
-    const handleDialogClose = () => {
-        setSelectedEvent(null);
-    };
+    const cellWidth = 107;
+    const eventWidth = 100;
 
     return (
-        <div className="calendar-container">
-            {Array.from({ length: 4 }).map((_, weekIndex) => (
-                <div key={weekIndex} className="week">
-                    {Array.from({ length: 7 }).map((_, dayIndex) => {
-                        const date = new Date();
-                        date.setDate(1 + weekIndex * 7 + dayIndex);
+        <Fragment>
+            <h1>События</h1>
 
-                        const dayEvents = events.filter(
-                            (event) =>
-                                event.start.getDate() === date.getDate() &&
-                                event.start.getMonth() === date.getMonth()
-                        );
+            <div className="eventsButtons">
+                <Stack direction="row" spacing={1} className="changePeriodButtons">
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        onClick={() => alert("Вернулся на сегодлняшний день в календаре")}
+                    >
+                        Сегодня
+                    </Button>
+                    <IconButton
+                        aria-label="previousDatePeriod"
+                        color="default"
+                        onClick={() => alert('test2')}
+                    >
+                        <ArrowBackIosNewIcon
+                            fontSize='small'/>
+                    </IconButton>
+                    <IconButton
+                        aria-label="nextDatePeriod"
+                        color="default"
+                        onClick={() => alert('test3')}
+                    >
+                        <ArrowForwardIosIcon
+                            fontSize='small' />
+                    </IconButton>
+                    <h2 style={{ color: "dimgray" }}>
+                        {"Декабрь"} {2024}
+                    </h2>
 
-                        return (
-                            <div
-                                key={dayIndex}
-                                className={`day ${dayEvents.length > 0 ? 'event-day' : ''}`}
-                                onClick={() => handleDayClick(date)}
-                            >
-                                {dayEvents.map((event) => (
-                                    <div
-                                        key={event.id}
-                                        className="event-indicator"
-                                        onClick={() => handleEventClick(event)}
-                                    ></div>
-                                ))}
-                                {date.getDate()}
-                            </div>
-                        );
-                    })}
-                </div>
-            ))}
+                </Stack>
+                <Stack direction="row-reverse" spacing={1} className="managePeriodButtons">
+                    <FormControl
+                        sx={{ minWidth: 60 }}
+                        size="small"
+                    >
+                        <Select
+                            value={2}
+                            onChange={() => alert('Функционал временно недоступен.')}
+                            inputProps={{ 'aria-label': 'Выбрать период' }}
+                        >
+                            <MenuItem value={1} disabled>Неделя</MenuItem>
+                            <MenuItem value={2}>Месяц</MenuItem>
+                            <MenuItem value={3} disabled>Год</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Stack>
+            </div>
 
-            {newEvent && (
-                <div className="event-form">
-                    <h2>Create Event</h2>
-                    <label>
-                        Event Title:
-                        <input
-                            type="text"
-                            name="title"
-                            value={newEvent.title}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                    <label>
-                        Start Date and Time:
-                        <input
-                            type="datetime-local"
-                            name="start"
-                            value={newEvent.start.toISOString().slice(0, -8)}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                    <label>
-                        Finish Date and Time:
-                        <input
-                            type="datetime-local"
-                            name="finish"
-                            value={newEvent.finish.toISOString().slice(0, -8)}
-                            onChange={handleInputChange}
-                        />
-                    </label>
-                    <button onClick={handleSaveEvent}>Save Event</button>
-                </div>
-            )}
+            <div className="mainCalendarGrid" style={{height: "120px", marginTop: "20px"}}>
+                {
+                    currentPeriod.map((week, index) => (
+                        //<div style="display: inline-block; border: 1px solid #ccc;">
+                        <div style={{
+                            display: "flex",
+                            position: "relative",
+                            height: "100%",
+                            minHeight: "40px",
+                            borderTop: "1px solid gray",
+                            borderLeft: "1px solid gray",
+                            textAlign: "justify",
+                            justifyContent: "space-between",
+                            textAlignLast: "justify"
+                        }}>
+                            {
+                                week.map((day, index) => (
+                                    <div style={{
+                                        display: "inline-block",
+                                        borderBottom: "1px solid gray",
+                                        borderRight: "1px solid gray",
+                                        width: "100%",
+                                        textAlign: "center"
+                                    }}>
+                                        { day.date }
+                                    </div>
+                                ))
+                            }
+                            {
+                                events.filter(x => x.startAt.getDate() >= week[0].date || x.finishAt.getDate() <= week[6].date).map((x, index) => (
+                                    <div style={{
+                                        position: "absolute",
+                                        marginTop: 30 + index * 20,
+                                        marginLeft: index * cellWidth,
+                                        width: eventWidth,
+                                        height: "10px",
+                                        background: "rgb(3, 155, 229)"
+                                    }}>
 
-            {selectedEvent && (
-                <div className="event-dialog">
-                    <h2>Event Details</h2>
-                    <p>Title: {selectedEvent.title}</p>
-                    <p>Start: {selectedEvent.start.toLocaleString()}</p>
-                    <p>Finish: {selectedEvent.finish.toLocaleString()}</p>
-                    <button onClick={handleDialogClose}>Close</button>
-                </div>
-            )}
-        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    ))
+                }
+            </div>
+
+
+        </Fragment>
     );
 }
 
